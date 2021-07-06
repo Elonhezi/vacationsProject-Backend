f44369b41ae411aa3e64d6411da86fa3a55af9fa
Elonhezi <elhezi119@gmail.com>
2021-07-06 16:49:36 +0300

    add time for token

--- helpers/jwt-helper.js
+++ helpers/jwt-helper.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken"); 
 
 function getNewToken(payload) { // (payload will be the user object)
-    // return jwt.sign({ payload }, config.jwtKey, { expiresIn: "30m" });
-    return jwt.sign({ payload }, config.jwtKey, { expiresIn: "30s" });
+    return jwt.sign({ payload }, config.jwtKey, { expiresIn: "30m" });
 }
 
 module.exports = {
